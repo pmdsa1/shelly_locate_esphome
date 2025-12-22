15555d4e75f29c41d4e67a61ee93bb9aedeac217
pmdsa1 <158192611+pmdsa1@users.noreply.github.com>
2025-12-22 21:27:05 +0000

    Update shelly_locate.cpp

--- components/shelly_locate/shelly_locate.cpp
+++ components/shelly_locate/shelly_locate.cpp
@@ -1,1 +1,9 @@
+#include "shelly_locate.h"
 
+namespace esphome {
+namespace shelly_locate {
+
+// Nothing needed here yet — your logic is fully in the header.
+
+}  // namespace shelly_locate
+}  // namespace esphome
