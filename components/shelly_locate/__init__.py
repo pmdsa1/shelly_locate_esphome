03e3a35288913e6ab204b063ba967fe3652e12f9
pmdsa1 <158192611+pmdsa1@users.noreply.github.com>
2025-12-22 22:49:34 +0000

    Update __init__.py

--- components/shelly_locate/__init__.py
+++ components/shelly_locate/__init__.py
@@ -1,1 +1,16 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import esp32_ble_tracker, esp32_ble_client
+from esphome.const import CONF_ID
 
+shelly_locate_ns = cg.esphome_ns.namespace("shelly_locate")
+ShellyLocate = shelly_locate_ns.class_("ShellyLocate", cg.Component, esp32_ble_client.BLEClientBase)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ShellyLocate),
+})
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
+    yield esp32_ble_client.register_ble_client(var)
